54be13f0765072b39e1a3955a5309fbf6f342d44
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-07-04 23:17:45 -0500

    Add axios get xd

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -1,9 +1,16 @@
+import axios from "axios";
 
 
 export const getMovies = async (page=1) => {
     try {
-        const resp = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=344fc7cfcfb692e19d65011f998c28d6&language=es-MX&page=${page}`)
-        const data = await resp.json();
+        const {data} = await axios.get('https://api.themoviedb.org/3/movie/popular',{
+            params:{
+                api_key:'344fc7cfcfb692e19d65011f998c28d6',
+                language:'es-MX',
+                page:page
+            },
+            headers:{}
+        })
         return data.results
     } catch (error) {
         console.log(error)
